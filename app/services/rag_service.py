30c4a62f5f14705d25df3e83d4e4471876f31c7f
Garvit Taneja <taneja.garvit29@gmail.com>
2026-04-18 23:27:50 +0530

    update Rag service (DB store)

---
+++ app/services/rag_service.py
@@ -0,0 +1,19 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from app.utils.embeddings import get_embeddings
+from app.utils.vector_db import save_vectorstore
+
+def process_document(file_path: str):
+    # load PDF
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+
+    # split into chunks
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50) # this was func for how we want to split
+    docs = splitter.split_documents(documents)
+
+    # embeddings
+    embeddings = get_embeddings()
+
+    # save into vector DB
+    save_vectorstore(docs, embeddings)
