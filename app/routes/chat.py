faeab45491707ed129d5ecbd9750b181a74bf6c8
Garvit Taneja <taneja.garvit29@gmail.com>
2026-04-19 16:11:56 +0530

    route update for stream-ask

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 from app.services.agent_service import run_agent
 
+from fastapi.responses import StreamingResponse
+from app.services.agent_service import stream_agent
+
 router = APIRouter()
 
 @router.post("/ask")
 def ask(query:ask):
     response = run_agent(query)
     return {"answer": response}
+
+@router.post("/stream-ask")
+def stream_ask(query: str):
+    generator = stream_agent(query)
+    return StreamingResponse(generator(), media_type="text/plain")
